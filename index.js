fd64ae8171efddc9218a82b1b50fe5303e048023
Paweł Wierciński <47865579+pawelwier@users.noreply.github.com>
2020-12-08 22:40:42 +0100

    Login/Register methods and validation

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
-const { MONGODB } = require('./config.js');
-
-const typeDefs = gql`
-    type Post{
-        id: ID!
-        body: String!
-        username: String!
-        createdAt: String!
-    }
-    type Query{
-        getPosts: [Post]
-
-    }
-`
-
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            } catch(error){
-                throw new Error(error)
-            }
-        }
-    }
-}
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers/index');
+const { MONGODB } = require('./config.js'); 
 
 const server = new ApolloServer({
     typeDefs,
